862a09747f073b57ea0d570fbc590ff41c132424
ErikPC <epardillo@cifpfbmoll.eu>
2022-04-04 17:58:25 +0200

    feat: añadir singleton a RoomTemperature

--- furnance/src/main/java/edu/poniperro/types/RoomTemperature.java
+++ furnance/src/main/java/edu/poniperro/types/RoomTemperature.java
@@ -2,9 +2,20 @@ package edu.poniperro.types;
 
 public class RoomTemperature {
     private double temperatura = 0;
+    private static RoomTemperature instance = null;
+
+    public RoomTemperature() {
+    };
 
     public RoomTemperature(double temperatura) {
         this.temperatura = temperatura;
+    }
+
+    public static RoomTemperature getInstance() {
+        if (instance == null) {
+            instance = new RoomTemperature();
+        }
+        return instance;
     }
 
     public double getTemperatura() {
