c62a3106238a977a8cd61c6772030967df64bdaf
ErikPC <epardillo@cifpfbmoll.eu>
2022-04-06 16:15:10 +0200

    feat: cabar clase pirata con su speak personalizado to guapo primo

--- furnance/src/main/java/edu/poniperro/App.java
+++ furnance/src/main/java/edu/poniperro/App.java
@@ -5,6 +5,7 @@ import edu.poniperro.hardware.Regulator;
 import edu.poniperro.interfaces.Heater;
 import edu.poniperro.interfaces.Thermometer;
 import edu.poniperro.otherstuff.Jedi;
+import edu.poniperro.otherstuff.Pirate;
 import edu.poniperro.types.RemoteCommandSensor;
 import edu.poniperro.types.RoomTemperature;
 
@@ -27,5 +28,10 @@ public class App {
         System.out.println("\nArrancando a Yoda: ");
         regulator.regulate(thermometer, yoda, minTemp, maxTemp, temperature);
         yoda.speak();
+
+        Pirate accino = new Pirate();
+        System.out.println("\nArrancando usuario de Atsu Atsu no mi:");
+        regulator.regulate(thermometer, accino, minTemp, maxTemp, temperature);
+        accino.speak();
     }
 }
